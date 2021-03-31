7feb8373c97700466e092c2bb4f435425cb1ade2
Jelte Dirks <j.dirks@outlook.com>
2021-03-31 12:29:03 +0200

    looping all files and having them as test names.

--- tests/debug.test.ts
+++ tests/debug.test.ts
@@ -2,28 +2,18 @@ import * as path from "path";
 import {recursiveJSONSearch} from "./helpers/recursiveJSONSearch";
 
 describe('polis', () => {
-
-  let filesToCheck: string[] = [];
   const sourcePath = path.resolve('./src');
+  let filesToCheck: string[] = recursiveJSONSearch(sourcePath);
 
-  beforeAll(() => {
-    filesToCheck = recursiveJSONSearch(sourcePath);
-  });
+  test.each(filesToCheck)('%s', (a) => {
+    let obj = require(a);
 
-  test('alle json bestanden', () => {
-    console.log(filesToCheck);
-
-    for (let i = 0; i < filesToCheck.length; i++) {
-      const obj: any = require(filesToCheck[i]);
-
-      if (Object.prototype.toString.call(obj) === '[object Array]') {
-        for (let j = 0; j < obj.length; j++) {
-          expect(obj[j]).toBeValidRegel(filesToCheck[i]);
-        }
-      } else {
-        fail('het hoofdobject in de json moet een array zijn, het is nu:' + Object.prototype.toString.call(obj));
+    if (Object.prototype.toString.call(obj) === '[object Array]') {
+      for (let j = 0; j < obj.length; j++) {
+        expect(obj[j]).toBeValidRegel(a);
       }
+    } else {
+      fail('het hoofdobject in de json moet een array zijn, het is nu:' + Object.prototype.toString.call(obj));
     }
   });
-
 });
